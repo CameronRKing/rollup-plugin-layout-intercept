94a30fc8df43ffd6d80d2036f32144b8bf40defb
Cameron King <cameronking42@gmail.com>
2020-08-04 18:51:36 -0500

    Adds commented-out code showing how I tried (and failed) to preserve both App state and editor layout.

--- src/LayoutWrapper.svelte
+++ src/LayoutWrapper.svelte
@@ -31,6 +31,16 @@ function getHooks(event) {
         .map(key => [key, store[key]]);
 }
 
+function runHooks(event) {
+    getHooks(event).forEach(([name, hook]) => {
+        if (typeof hook !== 'function') {
+            console.warn(`layout-intercept/${event}/${name} must be a function`);
+            return;
+        }
+        hook();
+    });
+}
+
 let layout, baseRow;
 onMount(async () => {
     const config = {
@@ -47,8 +57,8 @@ onMount(async () => {
         }]
     };
 
+    await loadScript('http://code.jquery.com/jquery-1.11.1.min.js');
     await Promise.all([
-        loadScript('http://code.jquery.com/jquery-1.11.1.min.js'),
         loadScript('https://golden-layout.com/files/latest/js/goldenlayout.min.js'),
         loadCss('https://golden-layout.com/files/latest/css/goldenlayout-base.css'),
         loadCss('https://golden-layout.com/files/latest/css/goldenlayout-light-theme.css')
@@ -58,11 +68,10 @@ onMount(async () => {
 
     // the IIFE is necessary because:
     // Svelte apparently won't catch the reference to SvelteBridge inside a normal function like this
-    // (keep getting 'SvelteBridge is undefined')
+    // (keep getting 'SvelteBridge is not a constructor')
     // GoldenLayout won't allow an anonymous function, because they can't be used as constructors.
     // So I need a normal function that has a working reference to SvelteBridge
     // an IIFE seemed appropriate
-    console.log('bridge is', SvelteBridge);
     layout.registerComponent('SvelteBridge', (function(SvelteBridge) {
         return function(container, componentState) {
             if (componentState.lm_title) container.setTitle(componentState.lm_title);
@@ -74,17 +83,86 @@ onMount(async () => {
         }
     })(SvelteBridge));
 
+
+    /**
+     * I tried for a few hours to switch between editing and normal views
+     * in a way that preserves both the current App state and the editor layout.
+     * But it seems as though GoldenLayout doesn't reattach all its event handlers
+     * when you work with it on such a low level.
+     * This code "works"; both App state and editor layout are preserved;
+     * however, NONE OF THE GOLDEN LAYOUT UI WORKS and I have no idea why.
+     *
+     * I give up.
+     * 
+     * I leave the code commented out in case I want to take another crack at it in the future.
+     **/
+    // window.l = layout;
+
+    // layout.registerComponent('AppPlaceholder', function(container, componentState) {
+    //     return {};
+    // });
+
+    // function findApp(root) {
+    //     return root.getItemsByFilter(item => item.isComponent && item.config.componentState.component == '/src/App')[0];
+    // }
+
+    // function findAppPlaceholder(root) {
+    //     return root.getItemsByFilter(item => item.isComponent && item.componentName == 'AppPlaceholder')[0];
+    // }
+
+    // function setRoot(root) {
+    //     // first, pull out the App component and mark where it was with a placeholder
+    //     const app = findApp(layout.root.contentItems[0]);
+    //     const appIdx = app.parent.contentItems.indexOf(app);
+    //     app.parent.addChild({ type: 'component', componentName: 'AppPlaceholder', componentState: {} }, appIdx);
+    //     app.parent.removeChild(app, true);
+
+    //     // then, replace the appless root with the new root
+    //     const oldRoot = layout.root.contentItems[0];
+    //     layout.root.removeChild(oldRoot, true);
+    //     oldRoot.element.remove();
+    //     layout.root.addChild(root);
+
+    //     // finally, add the App into the placeholder in the new root
+    //     const placeholder = findAppPlaceholder(root);
+    //     placeholder.parent.replaceChild(placeholder, app);
+    //     app.container.show();
+    // }
+
     setTimeout(() => layout.init(), 100);
     setTimeout(() => {
+        // // the easiest way to hydrate a second config is to add it
+        // // then remove it without destroying it
+        // const editingRoot = layout.root.contentItems[0];
+        // const app = findApp(editingRoot);
+        // const appIdx = app.parent.contentItems.indexOf(app);
+        // app.parent.addChild({ type: 'component', componentName: 'AppPlaceholder', componentState: {} }, appIdx);
+        // app.parent.removeChild(app, true);
+        // layout.root.removeChild(editingRoot, true);
+        // editingRoot.element.remove();
+
+        // layout.root.addChild({
+        //     type: 'row',
+        //     content: [{
+        //         type: 'component',
+        //         componentName: 'AppPlaceholder',
+        //         componentState: {},
+        //     }]
+        // });
+        // const normalRoot = layout.root.contentItems[0];
+        // const placeholder = findAppPlaceholder(normalRoot);
+        // placeholder.parent.replaceChild(placeholder, app);
+        // app.container.show();
+
         baseRow = layout.root.contentItems[0];
         const header = document.querySelector('.lm_header'),
             content = document.querySelector('.lm_content'),
-            base = document.querySelector('.lm_goldenlayout');
-
-        const contentBg = content.style.background,
+            base = document.querySelector('.lm_goldenlayout'),
+            contentBg = content.style.background,
             baseBg = base.style.background;
 
         window.hideLayout = () => {
+            // assume the App is in a single column on the rightmost side
             while (baseRow.contentItems.length > 1) {
                 baseRow.removeChild(baseRow.contentItems[0]);
             }
@@ -94,21 +172,21 @@ onMount(async () => {
             base.style.background = 'transparent';
         }
 
+        let firstTime = true;
         window.showLayout = () => {
-            getHooks('onShow').forEach(([name, hook]) => {
-                if (typeof hook !== 'function') {
-                    console.warn(name + ' must be a function');
-                    return;
-                }
-                hook();
-            });
+            if (firstTime) {
+                firstTime = false;
+                runHooks('onInit');
+            }
+
+            runHooks('onShow');
 
             header.style.display = 'block';
             content.style.background = contentBg;
             base.style.background = baseBg;
         };
 
-        window.hideLayout();
+        hideLayout();
     }, 200);
 });
 
